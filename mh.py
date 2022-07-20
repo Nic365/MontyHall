6ed842768fad48656a38db0bcb9352c39adecaf0
Nicolas Payre <nicolas.payre@cegepsherbrooke.qc.ca>
2022-07-19 21:44:14 -0400

    version sans tableau finalement

--- mh.py
+++ mh.py
@@ -2,30 +2,39 @@ import random
 
 class Game:
     def __init__(self):
+        self.choice = None
+        self.rmdoor = None
         self.tresor = random.randint(0,2)
-        self.doors = [0, 0, 0]
-        self.doors[self.tresor] = 1
+        self.doors = ["_", "_", "_"]
+        self.doors[self.tresor] = "$"
 
     def make_choice(self, choice):
         self.choice = choice
 
     def check_result(self):
-        if self.doors[self.choice] == 1:
+        if self.doors[self.choice] == "$":
             return 1
         else:
             return 0
 
 
     def remove_door(self):
-        None
-        # retirer une porte qui n'est pas le tresor ni celle choisit
-        #  comment ?
-
+        n = random.randint(0,2)
+        while n == self.tresor or n == self.choice:            
+            n = random.randint(0,2)
+        self.rmdoor = n
+        self.doors[self.rmdoor] = "X"
+        
+    #def __str__(self):
+        #return self.doors
 
 # test
 
 g1 = Game()
 g1.make_choice(1)
+print(g1.doors)
+g1.remove_door()
+print(g1.doors)
 print(g1.check_result())
 
 """ rounds = 1000
