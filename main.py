03a71122d974f154e07bad2bc4f7db02c789d802
Nicolas Payre <nicolas.payre@cegepsherbrooke.qc.ca>
2022-07-20 22:34:49 -0400

    renamed files/update tests

---
+++ main.py
@@ -0,0 +1,49 @@
+from mymodule import Game
+import random
+
+# test 1 game
+def just_one_game(): 
+    g1 = Game()
+    print("new game:", g1.doors)
+    g1.make_choice(random.randint(0,2))
+    print("after choice:",g1.doors)
+    g1.remove_door()
+    print("after remove:",g1.doors)
+    g1.switch_choice()
+    print("after switch:",g1.doors)
+    print(g1.check_result())
+
+    if g1.check_result():
+        print("youhoo")
+    else:
+        print("boohoo")
+
+def bunch_of_games(howmany, switch):
+    win, lost = 0, 0
+    for i in range(1, howmany):
+        g1 = Game()
+        # choisit une porte au hasard
+        g1.make_choice(random.randint(0,2))
+        # retirer une porte
+        g1.remove_door()
+        # on veux-tu changer ? 
+        if switch:
+            g1.switch_choice()
+        # retenir le resultat
+        if g1.check_result():
+            win+=1
+        else:
+            lost+=1
+    
+    # stats
+    print("Win:", win)
+    print("lost:", lost)
+    print(round(100 * win/(win+lost),2),"%")
+
+
+#just_one_game()
+print("let's see a thousand games with no change")
+bunch_of_games(1000, 0)
+
+print("let's see a thousand games with F**** change")
+bunch_of_games(1000, 1)
--- mymodule.py
+++ mymodule.py
@@ -41,43 +41,5 @@ class Game:
     def __str__(self):
         return self.doors
 """
-# test
-
-g1 = Game()
-print("new game:", g1.doors)
-g1.make_choice(1)
-print("after choice:",g1.doors)
-g1.remove_door()
-print("after remove:",g1.doors)
-g1.switch_choice()
-print("after switch:",g1.doors)
-print(g1.check_result())
-
-if g1.check_result():
-    print("youhoo")
-else:
-    print("boohoo")
-
-
-""" rounds = 1000
-
-print("No change rounds")
-win, lost = 0, 0
-for i in range(1, rounds):
-    g1 = Game()
-    #print(g1.door1, g1.door2, g1.door3)
-    g1.make_choice(1)
-    g1.remove_door()
-
-    if g1.check_result():
-        win+=1
-    else:
-        lost+=1
-
-print("Win:", win)
-print("lost:", lost)
-print(round(100 * win/(win+lost),2),"%")
- """
-#Percentage = 100 * float(part)/float(whole)
 
 
