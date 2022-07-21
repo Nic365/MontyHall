b6adf59af2fd652c59921180a5c9fd7f4415de02
Nicolas Payre <nicolas.payre@cegepsherbrooke.qc.ca>
2022-07-20 21:12:54 -0400

    version qui fonctionne pas pire

--- mh.py
+++ mh.py
@@ -10,9 +10,11 @@ class Game:
 
     def make_choice(self, choice):
         self.choice = choice
+        self.doors[self.choice] = "*"
 
     def check_result(self):
-        if self.doors[self.choice] == "$":
+
+        if self.tresor == self.choice:
             return 1
         else:
             return 0
@@ -25,17 +27,37 @@ class Game:
         self.rmdoor = n
         self.doors[self.rmdoor] = "X"
         
-    #def __str__(self):
-        #return self.doors
+    def switch_choice(self):
+        n = random.randint(0,2)
+        while n == self.choice or n == self.rmdoor:
+            n = random.randint(0,2)
+        self.doors[self.choice] = "_"
+        self.choice = n
+        self.doors[self.choice] = "*"
 
+# could be nice to have a print function
+# to use like print(g1) but heh
+"""
+    def __str__(self):
+        return self.doors
+"""
 # test
 
 g1 = Game()
+print("new game:", g1.doors)
 g1.make_choice(1)
-print(g1.doors)
+print("after choice:",g1.doors)
 g1.remove_door()
-print(g1.doors)
+print("after remove:",g1.doors)
+g1.switch_choice()
+print("after switch:",g1.doors)
 print(g1.check_result())
+
+if g1.check_result():
+    print("youhoo")
+else:
+    print("boohoo")
+
 
 """ rounds = 1000
 
